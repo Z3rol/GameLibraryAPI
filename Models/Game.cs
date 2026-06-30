b7e3668a7a63f8ba94f0952a3f914be303d8201f
Zerol <denisdeneroxperia@gmail.com>
2026-06-30 22:03:44 +0300

    Add Game model
    - Rm cached .http

---
+++ Models/Game.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameLibraryAPI.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public string DeveloperName { get; set; } = "";
+        public double Price { get; set; }
+        public DateTime ReleasedOn { get; set; }
+    }
+}
