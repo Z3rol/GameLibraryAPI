fc4dfff6347fbd7d4d044e425d1b70b3e679a012
Zerol <denisdeneroxperia@gmail.com>
2026-07-11 00:01:45 +0300

    feat: add validation rules to update game details dto

--- DTOs/Game/UpdateGameDetailsDto.cs
+++ DTOs/Game/UpdateGameDetailsDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace GameLibraryAPI.DTOs.Game
 {
     public class UpdateGameDetailsDto
     {
+        [Required]
+        [StringLength(150, MinimumLength = 1, ErrorMessage = "Game name must be between 1 and 150 characters.")]
         public string Name { get; set; } = "";
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Game genre must be between 1 and 50 characters.")]
         public string Genre { get; set; } = "";
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Game developer's name must be between 1 and 100 characters.")]
         public string DeveloperName { get; set; } = "";
+        [Required(ErrorMessage = "Release date is required.")]
         public DateOnly ReleasedOn { get; set; }
     }
 }
