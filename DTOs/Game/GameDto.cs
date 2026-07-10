fad1d4c0f1b6e2a1fcda5507066c2c7ba856f976
Zerol <denisdeneroxperia@gmail.com>
2026-07-10 23:39:47 +0300

    refactor: remove unnecessary range validation

--- DTOs/Game/GameDto.cs
+++ DTOs/Game/GameDto.cs
@@ -12,8 +12,6 @@ namespace GameLibraryAPI.DTOs.Game
         public string Name { get; set; } = "";
         public string Genre { get; set; } = "";
         public string DeveloperName { get; set; } = "";
-        [Required]
-        [Range(0.0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0")]
         public double Price { get; set; }
         public DateOnly ReleasedOn { get; set; }
     }
