a34da438b8dc2a2069c3c3768e8d1e1da0b97447
Zerol <denisdeneroxperia@gmail.com>
2026-08-20 04:11:39 +0300

    fix: change token expiration to use UtcNow, instead of Now

--- GameLibraryAPI/Services/TokenService.cs
+++ GameLibraryAPI/Services/TokenService.cs
@@ -35,7 +35,7 @@ namespace GameLibraryAPI.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
+                Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = creds,
                 Issuer = _config["JWT:Issuer"],
                 Audience = _config["JWT:Audience"]
